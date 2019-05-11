ad79c46a78b084493cd29049b0d5ff44a8355bfb
mike.jurovcik <skylitsoldier13@yahoo.com>
2019-05-10 23:49:13 -0400

    Created test C++ level.

---
+++ FF7_Battle_Sim/Source/FF7_Battle_Sim/OpeningLevel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "OpeningLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FF7_BATTLE_SIM_API AOpeningLevel : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
