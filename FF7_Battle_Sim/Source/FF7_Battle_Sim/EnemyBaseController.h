2231861aba12e62093b69e11d040ecadec05ae71
mike.jurovcik <skylitsoldier13@yahoo.com>
2019-06-04 04:33:14 -0400

    Created class for the Base AI Controller for enemies. Will have them have an ATB bar for actions as well.

---
+++ FF7_Battle_Sim/Source/FF7_Battle_Sim/EnemyBaseController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyBaseController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FF7_BATTLE_SIM_API AEnemyBaseController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
