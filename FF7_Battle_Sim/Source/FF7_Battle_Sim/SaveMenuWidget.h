05979129fd65bd7a473d9ad366b404611506d148
mike.jurovcik <skylitsoldier13@yahoo.com>
2019-06-11 03:04:41 -0400

    Create a SaveObject class and began making a save menu.

---
+++ FF7_Battle_Sim/Source/FF7_Battle_Sim/SaveMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SaveMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FF7_BATTLE_SIM_API USaveMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
